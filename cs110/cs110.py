37432b5dad0c6d4a82d8d9039143a717bcf5e477
phb <pbucci@gmail.com>
2024-04-21 14:06:59 -0700

    disable

--- cs110/cs110.py
+++ cs110/cs110.py
@@ -138,8 +138,8 @@ def lint(filename: str) -> None:
     print(blue(message))
 
     reporter = ColorizedTextReporter()
-    results = Run(["--disable C0103, C0303, C0304, R1732, R0903",
-                   "--ignore test-repo"
+    results = Run(["--disable=C0103, C0303, C0304, R1732, R0903",
+                   "--ignore='test-repo'"
                    filename], reporter=reporter, exit=False)
     if results.linter.stats.global_note < 9.0:
         raise Exception("Too many linting errors.") #pylint: disable=broad-exception-raised
