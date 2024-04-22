94f0b54b11808d3d036b61bf96a12ce55c0775ba
phb <pbucci@gmail.com>
2024-04-21 18:12:54 -0700

    disable

--- cs110/cs110.py
+++ cs110/cs110.py
@@ -118,7 +118,7 @@ def typecheck(file_path: str) -> None:
     print(blue(header(message)))
     print(blue(message))
     
-    command = ['mypy', "--disallow-untyped-defs", "--exclude=.git/, .pylintrc", file_path]
+    command = ['mypy', "--disallow-untyped-defs", file_path]
     result = subprocess.run(command, text=True, capture_output=True)
 
     highlighted = re.sub(r"\berror\b", r"\033[91merror\033[0m", result.stdout)
